530a0cd208109a73bfdbe6f856bbddd67678a9f8
ronan-keane <55560863+ronan-keane@users.noreply.github.com>
2024-02-28 11:24:20 -0500

    Update __init__.py

--- havsim/__init__.py
+++ havsim/__init__.py
@@ -13,7 +13,7 @@ from havsim import old
 
 # base classes/functions
 from havsim.simulation import Simulation
-from havsim.vehicles import Vehicle
+from havsim.vehicles import Vehicle, CrashesStochasticVehicle
 from havsim.road import Lane, Road
 from havsim.road import get_headway, get_dist
 from havsim.vehicles import reload

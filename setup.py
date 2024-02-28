4167500c9044bb040a2afdcfb95760cbc58d6618
ronan-keane <55560863+ronan-keane@users.noreply.github.com>
2024-02-28 15:26:05 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
                 'License :: Apache 2.0',
                 'Operating System :: Windows/Linux/Mac'
         ],
-        install_requires=['palettable', 'scipy', 'bayesian-optimization'],
+        install_requires=['palettable', 'scipy', 'bayesian-optimization', 'tqdm'],
         python_requires = '>=3.10',
         packages = setuptools.find_packages(exclude = ['Jax','nlopt','scripts', 'tensorflow', 'tensorflow-probability', 'networkx'])
         )

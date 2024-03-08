f9bdeb4b21b2091dcd78da7d5539bf2feb395578
ronan-keane <55560863+ronan-keane@users.noreply.github.com>
2024-03-08 12:11:02 -0500

    update script args loader

--- havsim/helper.py
+++ havsim/helper.py
@@ -5,6 +5,7 @@ import numpy as np
 import math
 import argparse
 import ast
+import inspect
 
 # TODO - fix code style and documentation
 
@@ -214,24 +215,28 @@ def load_dataset(dataset, column_dict={'veh_id': 0, 'frame_id': 1, 'lane': 13, '
     return all_veh_dict
 
 
-def script_args_helper(arg_names, default_args=None, description_str=None, arg_descriptions=None):
+def script_args_helper(arg_names, default_args=None, description_str=None, arg_descriptions=None, n_pos_args=None):
     """Implements extra functionality when calling scripts from a python console.
 
     This uses the package argparse to automatically generate docstring for the script when given -help/-h option.
     This also implements the ability to use keyword arguments when calling scripts, so scripts may accept
-    keyword arguments with syntax as if it were a python function.
+    keyword arguments with syntax as if it were a python function. Various checks are done so that
+    a suitable error message can be given in case the call fails.
 
     Args:
         arg_names: list of str names, the names of the variables which should be defined for the script, in order.
         default_args: list of default values for any keyword arguments. must be the same length or shorter
-            than arg_names. Any variables which do not have default values are (required) positional arguments.
+            than arg_names. Any variables which do not have default values are assumed to be positional arguments.
             Note that any positional arguments must come first in the order.
-        description_str: str to set as description in -help/-h docstring
-        arg_descriptions: list with same length as arg_names, giving a str description of the corresponding argument
+        description_str: None, or str to set as description in -help/-h docstring
+        arg_descriptions: None, or list with same length as arg_names, giving a str description of each argument
+        n_pos_args: if n_pos_args is None, the number of positional arguments is defined by comparing the length
+            of default_args and arg_names. This can manually specify the n_pos_args. In that case, we allow
+            default_args to be the same length as arg_names, which can be used to make the docstring a bit nicer.
     Returns:
         args: tuple of length arg_names, containing the ordered values to use for the script
     """
-    # determine number of positional/keyword arguments
+    # check inputs, determine number of positional/keyword arguments
     assert type(arg_names) == list or type(arg_names) == tuple
     max_pos_index = -1
     for count, arg in enumerate(arg_names):
@@ -241,20 +246,48 @@ def script_args_helper(arg_names, default_args=None, description_str=None, arg_d
             arg_names[count] = 'Undefined'
             max_pos_index = count
         else:
-            raise ValueError('Expected None or str for variable name, received '+str(arg))
+            raise ValueError('Expected None or str for variable name, received ' + str(arg))
     n_args = len(arg_names)
-    if default_args is not None:
-        assert type(default_args) == list or type(default_args) == tuple
-        assert len(default_args) <= n_args, 'Expected at most '+str(n_args) + \
-                                            ' default arguments, received '+str(len(default_args))
-        n_pos_args = n_args - len(default_args)
-        assert n_pos_args > max_pos_index, 'Argument at index '+str(max_pos_index)+' was not given a name' + \
-            ', but a name is required since that argument was requested to be a keyword argument.'
+    if n_pos_args is None:
+        if default_args is not None:
+            assert type(default_args) == list or type(default_args) == tuple
+            assert len(default_args) <= n_args, 'Expected at most '+str(n_args) + \
+                                                ' default arguments, received '+str(len(default_args))
+            n_pos_args = n_args - len(default_args)
+
+            def default_value(ind):
+                return default_args[ind - n_pos_args]
+        else:
+            n_pos_args = n_args
+
+            def default_value(ind):
+                return ind
+    elif type(n_pos_args) == int:
+        assert n_pos_args <= n_args
+        if default_args is not None:
+            assert type(default_args) == list or type(default_args) == tuple
+            if len(default_args) == n_args:
+
+                def default_value(ind):
+                    return default_args[ind]
+            elif len(default_args) == n_args - n_pos_args:
+
+                def default_value(ind):
+                    return default_args[ind - n_pos_args]
+            else:
+                raise ValueError('default_args must have ' + 'length '+str(n_args)+' or length '
+                                 + str(n_args-n_pos_args)+', got length '+str(len(default_args)))
+        else:
+
+            def default_value(ind):
+                return ind
     else:
-        n_pos_args = n_args
+        raise ValueError('Expected value with type int for n_pos_args, received '+str(n_pos_args))
+    assert n_pos_args > max_pos_index, 'Argument at index ' + str(max_pos_index) + ' was not given a name' + \
+        ', but a name is required since that argument was requested to be a keyword argument.'
 
     # create usage string for argparser help message
-    file_str = str(os.path.basename(__file__))
+    file_str = str(os.path.basename(inspect.stack()[1].filename))
     usage_str = file_str
     for count, arg in enumerate(arg_names):
         if count < n_pos_args:
@@ -262,14 +295,17 @@ def script_args_helper(arg_names, default_args=None, description_str=None, arg_d
         else:
             usage_str += ' ['+arg+']'
     usage_str += ' [-h]'
-    usage_str += '\n   '+str(n_pos_args)+' positional arguments, '+str(n_args-n_pos_args)+' keyword arguments.'
-    usage_str += ' Example: \''+file_str+' '
-    for arg in arg_names[:n_pos_args]:
-        usage_str += arg+' '
+    usage_str += '\n'+str(n_pos_args)+' positional arguments, '+str(n_args-n_pos_args)+' keyword arguments.'
+    usage_str += '\nExamples:  \'python '+file_str
+    pos_args_str, kw_args_str = '', ''
+    for count, arg in enumerate(arg_names[:n_pos_args]):
+        pos_args_str += ' '+str(default_value(count))
     if n_args-n_pos_args > 0:
         use_ind = n_pos_args+1 if n_args - n_pos_args > 1 else n_pos_args
-        usage_str += arg_names[use_ind]+'='+str(default_args[use_ind-n_pos_args])
-    usage_str += '\''
+        kw_args_str += arg_names[use_ind]+'='+str(default_value(use_ind))
+        usage_str += pos_args_str+' '+kw_args_str+'\', or \'python '+file_str+pos_args_str+'\'.'
+    else:
+        usage_str += pos_args_str+'\'.'
     if description_str is not None:
         assert type(description_str) == str
 
@@ -300,7 +336,7 @@ def script_args_helper(arg_names, default_args=None, description_str=None, arg_d
             flags.append(arg)
             continue
 
-        assert count < n_args, 'Too many arguments given. The arguments \''+str(args)+'\' were all set ' + \
+        assert count < n_args, 'Too many arguments given. The arguments \''+str(tuple(args))+'\' were all set ' + \
             ' and an additional argument '+arg+' was given.'
 
         cur_split = arg.split('=', 1)
@@ -310,7 +346,7 @@ def script_args_helper(arg_names, default_args=None, description_str=None, arg_d
 
             # check if kwarg is valid
             assert arg_name in arg_name_to_ind, 'Received keyword argument \''+arg+'\' but \''+arg_name+'\' is ' + \
-                'not a valid argument name. Should be one of: '+arg_names
+                'not a valid argument name. Should be one of: '+str(arg_names)
             if arg_name in kwargs:
                 raise ValueError('Received multiple keyword arguments for \''+arg_name+'\'')
             if arg_exists[arg_name_to_ind[arg_name]]:
@@ -332,25 +368,35 @@ def script_args_helper(arg_names, default_args=None, description_str=None, arg_d
             arg_exists[count] = True
         count += 1
 
-    # add any default arguments, verify that all required arguments were given, and give args to argparser
-    # todo fix messages/error behavior
-    # todo need to pass the flags to parser
+    # verify that all required arguments were given, and give args to argparser
     assert sum(arg_exists[:n_pos_args]) == n_pos_args, 'Missing positional arguments. Expected values for \'' + \
         '\', \''.join(arg_names[:n_pos_args])+'\', but received '+', '.join(map(str, args[:n_pos_args]))
-    is_input_str = []
+    is_default_arg = []
     for count, arg_name in enumerate(arg_names):
         if arg_exists[count]:  # positional argument
-            is_input_str.append(True)
+            is_default_arg.append(False)
         elif arg_name in kwargs:  # keyword argument
             args[count] = kwargs[arg_name]
-            is_input_str.append(True)
+            is_default_arg.append(False)
         else:  # optional argument taking default value
-            args[count] = default_args[count-n_pos_args]  # todo argparse wants a str, so pass the index as a str
-            is_input_str.append(False)
+            args[count] = str('unused')
+            is_default_arg.append(True)
 
-    args = vars(parser.parse_args(args))
-    return (ast.literal_eval(args[arg_name]) if is_input_str[count] else args[arg_name]
-            for count, arg_name in enumerate(arg_names))
+    args.extend(flags)
+    args_dict = vars(parser.parse_args(args))
+    args = [None]*n_args
+    for count, arg_name in enumerate(arg_names):
+        if is_default_arg[count]:
+            args[count] = default_value(count)
+        else:
+            try:
+                ast.literal_eval(args_dict[arg_name])
+            except ValueError:
+                print('Could not evaluate input. If due to \'malformed node or string\', this error is probably '
+                      'caused by missing dashes (missing \' \' or " ") on the input.', file=sys.stderr)
+                raise
+            args[count] = ast.literal_eval(args_dict[arg_name])
+    return args
 
 
 class VehicleData:
